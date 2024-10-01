530391e339be72a9a0a92f1b7a2207dd512f72e2
the control group <105938503+DragonlordNewb@users.noreply.github.com>
2024-10-01 08:37:00 -0500

    added a bunch

--- spacetime.py
+++ spacetime.py
@@ -22,7 +22,7 @@ class ChristoffelSymbols:
 	def getDerivative(self, i, k, l, wrt):
 		if self.symbol_derivatives[i][k][l][wrt] is None:
 			self.symbol_derivatives[i][k][l][wrt] = diff(self.symbol[i][k][l], metric.x[wrt])
-		return self.symbol_derivatives[i][k][l]
+		return self.symbol_derivatives[i][k][l][wrt]
 
 class MetricTensor:
 
@@ -31,9 +31,12 @@ class MetricTensor:
 	x2 = None
 	x3 = None
 	x = None
+	v = symbols("v0 v1 v2 v3")
+	v0, v1, v2, v3 = v
 	tensor = None
 	tensor_inverse = None
 	tensor_derivatives = [[[None for i in range(4)] for j in range(4)] for k in range(4)]
+	tensor_mixed_derivatives = [[[[None for i in range(4)] for j in range(4)] for k in range(4)] for l in range(4)]
 	tensor_inverse_derivatives = [[[None for i in range(4)] for j in range(4)] for k in range(4)]
 
 	def __init__(self, coordinates: list[Symbol], metric: list[list["Expression"]]):
@@ -42,47 +45,83 @@ class MetricTensor:
 		self.tensor = metric
 		self.tensor_inverse = Matrix(metric).inv().tolist()
 
+	def get_dd(self, i, j):
+		return self.tensor[i][j]
+
+	def get_uu(self, i, j):
+		return self.tensor_inverse[i][j]
+
 	def getDerivative(self, mu: int, nu: int, wrt: Symbol) -> "Expression":
 		if self.tensor_derivatives[mu][nu][wrt] is None:
 			self.tensor_derivatives[mu][nu][wrt] = diff(self.tensor[mu][nu], self.x[wrt])
 		return self.tensor_derivatives[mu][nu][wrt]
+
+	def getMixedDerivative(self, mu: int, nu: int, wrt1: int, wrt2: int):
+		if self.tensor_mixed_derivatives[mu][nu][wrt1][wrt2] is None:
+			self.tensor_mixed_derivatives[mu][nu][wrt1][wrt2] = diff(diff(self.tensor[mu][nu], self.x[wrt1]), self.x[wrt2])
 
 	def getInverseDerivative(self, mu: int, nu: int, wrt: Symbol) -> "Expression":
 		if self.tensor_inverse_derivatives[mu][nu][wrt] is None:
 			self.tensor_inverse_derivatives[mu][nu][wrt] = diff(self.tensor_inverse[mu][nu], self.x[wrt])
 		return self.tensor_inverse_derivatives[mu][nu][wrt]
 
+class RiemannTensor:
+
+	metric: MetricTensor
+	tensor_uddd = [[[[None for i in range(4)] for j in range(4)] for k in range(4)] for l in range(4)]
+
+	def __init__(self, metric: MetricTensor) -> None:
+		self.metric = metric
+
+	def get(self, i: int, k: int, l: int, m: int):
+		if self.tensor_uddd[i][k][l][m] is None:
+			self.tensor_uddd[i][k][l][m] = (self.metric.getMixedDerivative(i,m,k,l)+self.metric.getMixedDerivative(k,l,i,m)-self.metric.getMixedDerivative(i,l,k,m)-self.metric.getMixedDerivative(k,m,i,l)) / 2
+		return self.tensor_uddd[i][k][l][m]
+
 class RicciTensor:
 
 	metric = None
 	christoffel = None
+	riemann = None
+	initializedWith = None
 	tensor_dd = [[None for i in range(4)] for j in range(4)]
 	tensor_uu = [[None for i in range(4)] for j in range(4)]
 	scalar = None
 
-	def __init__(self, christoffel: ChristoffelSymbols, metric: MetricTensor) -> None:
-		self.metric, self.christoffel = metric, christoffel
+	def __init__(self, initializer, metric: MetricTensor) -> None:
+		self.metric = metric
+		if type(initializer) == ChristoffelSymbols:
+			self.christoffel = initializer
+			self.initializedWith = ChristoffelSymbols
+		else:
+			self.riemann = initializer
+			self.initializedWith = RiemannTensor
 	
 	def get_dd(self, i: int, j: int):
 		if self.tensor_dd[i][j] is None:
-			self.tensor_dd[i][j] = sum(
-				self.christoffel.getDerivative(a, i, j, a) for a in range(4)
-			) - sum(
-				self.chrisoffel.getDerivative(a, a, i, j) for a in range(4)
-			) + sum(
-				sum(
-					self.christoffel.get(a,a,b) * self.christoffel.get(b,i,j)
-					- self.christoffel.get(a,i,b) * self.christoffel.get(b,a,j)
-					for b in range(4)
-				) for a in range(4)
-			)
+			if self.initializedWith == ChristoffelSymbols:
+				self.tensor_dd[i][j] = sum(
+					self.christoffel.getDerivative(a, i, j, a) for a in range(4)
+				) - sum(
+					self.chrisoffel.getDerivative(a, a, i, j) for a in range(4)
+				) + sum(
+					sum(
+						self.christoffel.get(a,a,b) * self.christoffel.get(b,i,j)
+						- self.christoffel.get(a,i,b) * self.christoffel.get(b,a,j)
+						for b in range(4)
+					) for a in range(4)
+				)
+			else:
+				self.tensor_dd[i][j] = sum(
+					self.riemann.get(k, i, k, j) for k in range(4)
+				)
 		return self.tensor_dd[i][j]
 
 	def get_uu(self, i: int, j: int):
 		if self.tensor_uu[i][j] is None:
 			self.tensor_uu[i][j] = sum(
 				sum(
-					self.metric.getInverse(i, a) * self.metric.getInverse(j, b) * self.get_dd(a, b)
+					self.metric.get_uu(i, a) * self.metric.get_uu(j, b) * self.get_dd(a, b)
 					for b in range(4)
 				) for a in range(4)
 			)
@@ -92,7 +131,7 @@ class RicciTensor:
 		if self.scalar is None:
 			self.scalar = sum(
 				sum(
-					self.metric.getInverse(i, j) * self.get_dd(i, j)
+					self.metric.get_uu(i, j) * self.get_dd(i, j)
 					for j in range(4)
 				) for i in range(4)
 			)
@@ -111,7 +150,7 @@ class EinsteinTensor:
 	def get_dd(self, i: int, j: int):
 		if self.tensor_dd[i][j] is None:
 			self.tensor_dd[i][j] = self.ricci.get_dd(i, j) \
-			- (self.ricci.metric.get(i, j) * self.ricci.getScalar() / 2)
+			- (self.ricci.metric.get_dd(i, j) * self.ricci.getScalar() / 2)
 		return self.tensor_dd[i][j]
 
 	def get_uu(self, i: int, j: int):
@@ -119,6 +158,58 @@ class EinsteinTensor:
 			self.tensor_uu[i][j] = self.ricci.get_uu(i, j) \
 			- (self.ricci.metric.getInverse(i, j) * self.ricci.getScalar() / 2)
 		return self.tensor_uu[i][j]
+
+class StressEnergyMomentumTensor:
+
+	einstein: EinsteinTensor = None
+	units: "UnitSystem" = None
+	tensor_dd = [[None for i in range(4)] for j in range(4)]
+	tensor_uu = [[None for i in range(4)] for j in range(4)]
+
+	def __init__(self, einstein: EinsteinTensor, units: "UnitSystem") -> None:
+		self.einstein = einstein
+		self.units = units
+
+	def get_dd(self, i: int, j: int):
+		if self.tensor_dd[i][j] is None:
+			self.tensor_dd[i][j] = (self.einstein.get_dd(i,j)+(self.units.Lambda*self.einstein.ricci.metric.get_dd(i,j)))/self.units.kappa
+		return self.tensor_dd[i][j]
+
+	def get_uu(self, i: int, j: int):
+		if self.tensor_uu[i][j] is None:
+			self.tensor_uu[i][j] = (self.einstein.get_dd(i,j)+(self.units.Lambda*self.einstein.ricci.metric.get_uu(i,j)))/self.units.kappa
+		return self.tensor_uu[i][j]
+
+class GeodesicAccelerationVector:
+
+	metric: MetricTensor = None
+	christoffel: ChristoffelSymbols
+	vector_dxds = [None for i in range(4)]
+	vector_dxdt = [None for i in range(4)]
+
+	def __init__(self, metric: MetricTensor, christoffel: ChristoffelSymbols):
+		self.metric = metric
+		self.christoffel = christoffel
+
+	def get_dxds(self, i: int):
+		if self.vector_dxds[i] is None:
+			self.vector_dxds[i] = -sum(
+				sum(
+					self.christoffel.get(i, a, b) * self.metric.v[a] * self.metric.v[b]
+					for b in range(4)
+				) for a in range(4)
+			)
+		return self.vector_dxds[i]
+
+	def get_dxdt(self, i: int):
+		if self.vector_dxdt[i] is None:
+			self.vector_dxdt[i] = sum(
+				sum(
+					self.christoffel.get(0, a, b) * self.metric.v[a] * self.metric.v[b] * self.metric.v[i]
+					for b in range(4)
+				) for a in range(4)
+			) - self.get_dxds(i)
+		return self.vector_dxdt[i]
 
 class UnitSystem:
 
@@ -132,8 +223,33 @@ class UnitSystem:
 		c = 1 if normc else Symbol("c")
 		G = 1 if normG else Symbol("G")
 		h = 1 if normh else Symbol("h")
-		Lambda = 1 if normLambda else Symbol("Lambda")
+		Lambda = 0 if normLambda else Symbol("Lambda")
 		kappa = 8 * pi * self.einstein_constant / self.lightspeed**4
+
+class Spacetime:
+
+	units: UnitSystem = None
+	metric: MetricTensor = None
+	christoffel: ChristoffelSymbols = None
+	riemann: RiemannTensor = None
+	ricci: RicciTensor = None
+	einstein: EinsteinTensor = None
+	stress_energy_momentum: StressEnergyMomentumTensor = None
+
+	geodesic_acceleration: GeodesicAccelerationVector = None
+
+	def __init__(self, metric: MetricTensor, units: UnitSystem, calculateRicciWith: type=ChristoffelSymbols):
+		self.metric = metric
+		self.units = units
+		self.christoffel = ChristoffelSymbols(self.metric)
+		self.riemann = RiemannTensor(self.metric)
+		if calculateRicciWith == ChristoffelSymbols:
+			self.ricci = RicciTensor(self.christoffel, self.metric)
+		else:
+			self.ricci = RicciTensor(self.riemann, self.metric)
+		self.einstein = EinsteinTensor(self.metric, self.ricci)
+		self.stress_energy_momentum = StressEnergyMomentumTensor(self.einstein, self.units)
+		self.geodesic_acceleration = GeodesicAccelerationVector(self.metric, self.christoffel)
 
 if __name__ == "__main__":
 	print("Testing Schwarzschild geometry")
