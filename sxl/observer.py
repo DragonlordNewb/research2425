42c1a0f5775b216ddc5d781fc8dd5b321f09a747
the control group <105938503+DragonlordNewb@users.noreply.github.com>
2024-10-08 08:23:24 -0500

    Started work on new observer engine

--- sxl/observer.py
+++ sxl/observer.py
@@ -6,48 +6,46 @@ class Observer:
 
     metric_tensor: spacetime.MetricTensor = None
     zero: spacetime.GeneralFourVector = None
-    position: spacetime.GeneralFourVector = None # Current position
-    velocity: spacetime.GeneralFourVector = None # Current linear velocity
-    angular_position: spacetime.GeneralFourVector = None # Current angular position
-    angular_velocity: spacetime.GeneralFourVector = None # Current angular velocity
+    position: spacetime.GeneralFourVector = None # Current position, X
+    proper_velocity: spacetime.GeneralFourVector = None # Current proper linear velocity, u=dX/dtau
+    angle: spacetime.GeneralFourVector = None # Current angular position, theta
+    proper_rotation: spacetime.GeneralFourVector = None # Current angular velocity, w=dtheta/dtau
 
     def __init__(self, metric: spacetime.MetricTensor, xi=None, vi=None, ti=None, wi=None):
         self.metric_tensor = metric
         self.zero = spacetime.GeneralFourVector(self.metric_tensor, "u", 0, 0, 0, 0)
         self.position = xi if xi is not None else self.zero
         self.velocity = vi if vi is not None else self.zero
-        self.angular_position = ti if ti is not None else self.zero
-        self.angular_velocity = wi if wi is not None else self.zero
+        self.angle = ti if ti is not None else self.zero
+        self.proper_rotation = wi if wi is not None else self.zero
 
-    def accelerate(self, acceleration: spacetime.GeneralFourVector, dt=DEFAULT_DT):
-        self.velocity += acceleration * dt
+    def lorentz_factor(self):
+        return self.proper_velocity.u(0)
 
-    def translate(self, dt=DEFAULT_DT):
-        self.position += self.velocity * dt
+    def apply_proper_acceleration(self, a: spacetime.GeneralFourVector, dtau: float):
+        """
+        Update proper velocity based on proper acceleration:
+        du = d(dX/dtau) = alpha dtau = (d^2X/dtau^2)dtau.
+        """
+        self.proper_velocity += a * dtau
 
-    def spin(self, acceleration: spacetime.GeneralFourVector, dt=DEFAULT_DT):
-        self.angular_velocity += acceleration * dt
-    
-    def rotate(self, dt=DEFAULT_DT):
-        self.angular_position += self.angular_velocity * dt
+    def apply_coordinate_acceleration(self, a: spacetime.GeneralFourVector, dt: float):
+        """
+        Update proper velocity based on coordinate acceleration:
+        du = d(dX/dtau) = a gamma^3 dtau = (d^2X/dt^2)(dt/dtau)^3 dtau.
+        """
+        self.proper_velocity += a * self.lorentz_factor()**3 * dt
 
-class ObserverEnsemble:
+    def apply_proper_time(self, dtau: float):
+        """
+        Update coordinate position based on elapsed proper time:
+        dX = u dtau = (dX/dtau)dtau.
+        """
+        self.position += self.proper_velocity * dtau
 
-    observers: list[Observer] = None
-
-    def __init__(self, *args, **kwargs):
-        if args == [] and kwargs == {}:
-            return
-        raise SyntaxError("Please initialize ObserverEnsemble with .collect() and .make() class methods.")
-    
-    @classmethod
-    def collect(cls, *observers):
-        result = cls()
-        if type(observers[0]) == list:
-            observers = observers[0]
-        result.observers = observers
-
-    @classmethod
-    def make(cls, positions, velocities, angular_positions, angular_velocities):
-        if len(set(map(len, [positions, velocities, angular_positions, angular_velocities]))) > 1:
-            raise SyntaxError("Invalid arguments. Use the same number of X, V, T, and W elements.")
+    def apply_coordinate_time(self, dt: float):
+        """
+        Update coordinate position based on elapsed coordinate time:
+        dX = u dt/gamma = (dX/dtau)(dtau/dt) dt
+        """
+        self.position += self.proper_velocity * dt / self.lorentz_factor()
