864270b861f41197d49cb3aeab8d78ed2477823b
DragonlordNewb <luxbodell@gmail.com>
2024-10-15 19:44:05 -0500

    Modified the integer maker thing

--- interferometer/v1/server.py
+++ interferometer/v1/server.py
@@ -64,7 +64,6 @@ if __name__ == "__main__":
 		serial_port.close()
 		exit(1)
 	print("Got connection from client at " + addr_str + ".")
-	println("Transmitting data live...\t\tNO FEED")
 
 	serial_port.readline()
 
@@ -78,7 +77,10 @@ if __name__ == "__main__":
 			while True:
 				if serial_port.in_waiting > 0:
 					data = serial_port.readline().decode("utf-8").strip()
-					num = float(data)
+					try:
+						num = float(data)
+					except:
+						print("ERROR\tERROR\t" + str(timeout) + "\t█")
 					if num == 0:
 						print("ERROR\t" + last + "\t" + str(timeout) + "\t█")
 					else:
@@ -94,37 +96,4 @@ if __name__ == "__main__":
 				tcp_socket.close()
 				serial_port.close()
 				print("Server systems closed and shut down.")
-
-if __name__ == "__main__":
-	if len(sys.argv) != 3:
-		print("Invalid syntax: \"python3 relay.py <target> <port>\"")
-		exit(-1)
-	print("Loading serial connection ...", end="")
-	sys.stdout.flush()
-	ser = serial.Serial(sys.argv[2], 9600)
-	print("done.\nLoading socket ...", end="")
-	sys.stdout.flush()
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	print("done, ready to transmit data.")
-
-	ser.readline()
-
-	while True:
-		try:
-			while True:
-				# Read data from Arduino
-				if ser.in_waiting > 0:
-					data = ser.readline().decode('utf-8').strip()
-					print(f"Received from interferometer: {data}")
-					
-					# Send data to TCP client
-					sock.sendto(data.encode('utf-8'), (sys.argv[1], 7777))
-				else:
-					print("No feed.")
-					time.sleep(.1)
-
-		except KeyboardInterrupt:
-			if input("Continue? (y/n) ") == "n":
-				sock.close()
-				ser.close()
-		
+				exit(0)
