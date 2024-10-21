26a74b193016d2e754e78e91538a6c043255050c
the control group <105938503+DragonlordNewb@users.noreply.github.com>
2024-10-21 18:39:51 +0000

    Yay, I calculated the LVLF components (mostly) right!

--- sxl_exec.py
+++ sxl_exec.py
@@ -14,4 +14,7 @@ print()
 
 print("Christoffel symbols of the second kind:")
 
+alpha = units.c**2 + sympy.Symbol("v")*sympy.Function("phi")(*coords.coordinates)
+
 sympy.pprint(st.christoffel_symbols.christoffel_symbols_udd)
+sympy.pprint(st.christoffel_symbols.udd(0, 0, 0).subs(sympy.Function("v_s")(coords.x(0)), sympy.Symbol("v")).subs(alpha, sympy.Symbol("alpha")))
