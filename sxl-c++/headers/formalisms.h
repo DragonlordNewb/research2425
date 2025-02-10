b57a9ab4aee18aff45f8a8515046a799bc7bf69c
the control group <105938503+DragonlordNewb@users.noreply.github.com>
2025-02-10 09:01:45 -0600

    Started on formalisms.h

---
+++ sxl-c++/headers/formalisms.h
@@ -0,0 +1,29 @@
+#include "geometry.h"
+
+namespace formalism {
+
+    class Formalism {
+
+        public:
+
+    };
+
+    // ===== IMPLEMENTATIONS ===== //
+
+    // === Weak 
+
+    class WeakFieldApproximation {
+
+    };
+
+    using LinearizedGravity = WeakFieldApproximation;
+
+    class ArnowittDeserMisner {
+
+    };
+
+    using ADM = ArnowittDeserMisner;
+
+    class 
+
+};
