ef24c194b5b5db94970655cce10c9479e92c8ff8
the control group <105938503+DragonlordNewb@users.noreply.github.com>
2025-02-12 18:23:48 +0000

    Added WFA Ricci tensor calculator, it should work I think

--- sxl-c++/headers/formalisms.h
+++ sxl-c++/headers/formalisms.h
@@ -2,40 +2,50 @@
 
 namespace formalism {
 
-    class Formalism {
-
-        public:
-
-            virtual geometry::MetricTensor makeMetric() = 0;
-
-    };
+    class Formalism {};
 
     // ===== IMPLEMENTATIONS ===== //
 
-    // === Weak 
+    // === Weak Field Approximation === //
 
-    class WeakFieldApproximation {
+    class WeakFieldApproximation: public Formalism {
 
         protected:
 
             geometry::Rank2Tensor perturbation;
-            geometry::MetricTensor metric;
+            geometry::MetricTensor minkowski;
+            geometry::CoordinateSystem coords;
 
         public:
 
-            geometry::MetricTensor makeMetric() override {
-                //
-            } 
+            tensors::RicciTensor ricciTensor() {
+                tensors::RicciTensor Rmn;
+                Expression val;
+                for (int i = 0; i < metric.dim(); i++) {
+                    for (int j = 0; j < metric.dim(); j++) {
+                        val = coords.ddx(coords.ddx(perturbation.scalar(), j), i) + (minkowski.contra(i, j) * coords.ddx(coords.ddx(perturbation.co(i, j), j), i));
+                        for (int k = 0; k < metric.dim(); k++) {
+                            val += coords.ddx(coords.ddx(perturbation.mixed(k, i), j), k) + coords.ddx(coords.ddx(perturbation.mixed(k, j), i), k)
+                        }
+                        Rmn.set_co({i, j}, val);
+                    }
+                }
+                return Rmn;
+            }
 
     };
 
     using LinearizedGravity = WeakFieldApproximation;
+
+    // === Arnowitt-Deser-Misner (ADM) Formalism === //
 
     class ArnowittDeserMisner {
 
     };
 
     using ADM = ArnowittDeserMisner;
+
+    // === Primary/Auxiliary Warp Field Formalism === //
 
     class WarpFieldFormalism {
 
